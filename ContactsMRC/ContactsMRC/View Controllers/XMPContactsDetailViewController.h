2a5d8d6cdab826cb9f0fc0d41031f4e9f3f76450
hai666l <0xconr@protonmail.com>
2020-08-13 01:33:54 -0500

    Storyboard Setup Created View Controllers

---
+++ ContactsMRC/ContactsMRC/View Controllers/XMPContactsDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  XMPContactsDetailViewController.h
+//  ContactsMRC
+//
+//  Created by conner on 8/11/20.
+//  Copyright © 2020 conner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XMPContactsDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ContactsMRC/ContactsMRC/View Controllers/XMPContactsDetailViewController.m
@@ -0,0 +1,32 @@
+//
+//  XMPContactsDetailViewController.m
+//  ContactsMRC
+//
+//  Created by conner on 8/11/20.
+//  Copyright © 2020 conner. All rights reserved.
+//
+
+#import "XMPContactsDetailViewController.h"
+
+@interface XMPContactsDetailViewController ()
+
+@end
+
+@implementation XMPContactsDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
