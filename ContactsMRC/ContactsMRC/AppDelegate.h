67bd927f2cfe4fe4e378604f6b06ff7adcba9477
hai666l <0xconr@protonmail.com>
2020-08-13 00:53:25 -0500

    Created Project

---
+++ ContactsMRC/ContactsMRC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ContactsMRC
+//
+//  Created by conner on 8/11/20.
+//  Copyright © 2020 conner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ContactsMRC/ContactsMRC/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  ContactsMRC
+//
+//  Created by conner on 8/11/20.
+//  Copyright © 2020 conner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
